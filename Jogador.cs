7af0bdbd8d2b02b632564191b307b54650d5a71b
Eduardo Leal Aruth <eduardolealaruth@gmail.com>
2025-03-24 09:36:25 -0300

    feat: método de criar o dicionário
    Basicamente no método eu crio um dicionário com os jogaadores, depois eu crio uma string pra pegar o retorno da função ListarJogadores. O primeiro split (fora do foreach) é para separar cada jogador em uma posição diferente do vetor, o segundo (dentro do foreach) é para pegar cada jogador individualmente e adicionar no dicionário, faço o outro método no meu almoço

--- Jogador.cs
+++ Jogador.cs
@@ -9,9 +9,25 @@ namespace king_me
     class Jogador
     {
         public Jogador() { }
+        
+        private Dictionary<string,string> CriarDicionarioJogadores(int idPartida)
+        {
+            Dictionary<string, string> jogadoresDict = new Dictionary<string, string>();
+            string jogadoresString = KingMeServer.Jogo.ListarJogadores(idPartida);
+            string[] totalJogadores = jogadoresString.Split(';');
+            foreach (string jogador in totalJogadores)
+            {
+                string[] jogadorArray = jogador.Split(',');
+                jogadoresDict.Add(jogadorArray[0], jogadorArray[1]);
+            }
 
-
-
+            return jogadoresDict;
+        }
+        public static Dictionary<string, string> GetJogadorDaVez(int idPartida, int TKey) 
+        { 
+            
+        }
+        
         
     }
 }
